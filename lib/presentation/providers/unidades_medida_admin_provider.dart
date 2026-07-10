9d72e9bb38c40d18d04b320464a76960a384b9fd
mvzurita10 <mikyzurita65@gmail.com>
2026-07-10 09:20:28 -0500

    Update unidades_medida_admin_provider

---
+++ lib/presentation/providers/unidades_medida_admin_provider.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../domain/model/unidad_medida.dart';
+import '../../data/repository/unidad_medida_repository_impl.dart';
+
+class UnidadesMedidaAdminNotifier extends AsyncNotifier<List<UnidadMedida>> {
+    @override
+    Future<List<UnidadMedida>> build() async {
+        return _fetchUnidades();
+    }
+
+    Future<List<UnidadMedida>> _fetchUnidades() async {
+        final repository = ref.read(unidadMedidaRepositoryProvider);
+        return await repository.getUnidadesMedida();
+    }
+
+  // Agregamos los métodos de mutación que faltaban para el panel de admin
+    Future<void> addUnidadMedida(UnidadMedida unidad) async {
+        state = const AsyncValue.loading();
+        state = await AsyncValue.guard(() async {
+        // Asumiendo que agregas createUnidadMedida en tu repository
+        // await ref.read(unidadMedidaRepositoryProvider).createUnidadMedida(unidad);
+        return _fetchUnidades();
+        });
+    }
+
+    Future<void> updateUnidadMedida(UnidadMedida unidad) async {
+        state = const AsyncValue.loading();
+        state = await AsyncValue.guard(() async {
+        // await ref.read(unidadMedidaRepositoryProvider).updateUnidadMedida(unidad);
+        return _fetchUnidades();
+        });
+    }
+
+    Future<void> deleteUnidadMedida(String id) async {
+        state = const AsyncValue.loading();
+        state = await AsyncValue.guard(() async {
+        // await ref.read(unidadMedidaRepositoryProvider).deleteUnidadMedida(id);
+        return _fetchUnidades();
+        });
+    }
+}
+
+final unidadesMedidaAdminProvider = AsyncNotifierProvider<UnidadesMedidaAdminNotifier, List<UnidadMedida>>(() {
+    return UnidadesMedidaAdminNotifier();
+});
